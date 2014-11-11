51ec54356a50748a8f31bc7421ff6c7c007ccf56
Thomas Boggs <thomas.boggs@gmail.com>
2014-11-11 11:45:30 -0500

    Added default directories and cleaned code.

--- local/latex-functions.el
+++ local/latex-functions.el
@@ -1,37 +1,44 @@
 ;; Custom LaTeX functions
 
+(defvar *gmu-latex-root* "../"
+  "The assumed root directory where the main LaTeX file sits. This
+   affects how included graphics files are referenced in the files.")
+
+(defvar *gmu-latex-fig-dir* "../figures/"
+  "Default location of figures directory.")
 
 (defun gmu-figure (filename caption label)
-  "Inserts LaTeX template for a figure in GMU PhD dissertation format."
-;;  (interactive "sFile name: ")
+  "Inserts LaTeX template for a figure in GMU PhD dissertation format.
+   The file path will be relative to *gmu-latex-root*."
   (interactive (list
 		 (file-name-sans-extension
-		   (file-relative-name (read-file-name "File name:") ".."))
+		   (file-relative-name (read-file-name "File name:"
+						       *gmu-latex-fig-dir*)
+				       *gmu-latex-root*))
 		 (read-string "Caption: ")
 		 (read-string "LaTeX label: ")))
-  (insert (format "\\begin{figure}
+  (let ((label-tex (if (> (length label) 0)
+		       (format "  \\label{fig:%s}\n" label) "")))
+    (insert (format "\\begin{figure}
   \\centering
   \\includegraphics[width=0.5\\linewidth]{%s}
-  \\caption{%s}\n" filename caption))
-  (if (> (length label) 0)
-      (insert (format "  \\label{fig:%s}\n" label)))
-  (insert "  \\figSpace
-\\end{figure}\n\n"))
+  \\caption{%s}
+%s  \\figSpace
+\\end{figure}\n\n" filename caption label-tex))))
 
 (defun gmu-multifigure (numfigs caption label)
   "Inserts LaTeX template for a figure with multiple subfigures in GMU PhD dissertation format."
-;;  (interactive "sFile name: ")
   (interactive (list
 		 (read-number "Number of subfigures: ")
 		 (read-string "Caption: ")
 		 (read-string "LaTeX label: ")))
-  (let ((pos (point)))
+  (let ((pos (point))
+	(figscale (/ 0.95 numfigs)))
     (insert "\\begin{figure}\n  \\centering\n")
-    (let ((figscale (/ 1.0 numfigs)))
-      (dotimes (i numfigs)
-	(insert (format
-		  "  \\subcaptionbox{}[%.2f\\linewidth]{\\includegraphics[width=%.2f\\linewidth]{}}\n"
-		  figscale figscale))))
+    (dotimes (i numfigs)
+      (insert (format
+		"  \\subcaptionbox{}[%.2f\\linewidth]{\\includegraphics[width=%.2f\\linewidth]{}}\n"
+		figscale figscale)))
     (insert (format "  \\caption{%s}\n" caption))
     (if (> (length label) 0)
 	(insert (format "  \\label{fig:%s}\n" label)))
@@ -40,33 +47,38 @@
     (forward-line 2)
     (forward-char 17)))
 
-
 (defun gmu-table (numcols caption label)
   "Inserts LaTeX template for a table in GMU PhD dissertation format."
   (interactive (list
 		 (read-number "Number of columns: ")
 		 (read-string "Caption: ")
 		 (read-string "LaTeX label: ")))
-  (let ((pos (point)))
-    (insert (format "\\begin{table}\n  \\centering\n  \\caption{%s}\n" caption))
-    (insert "  \\begin{tabular}{|")
-    (dotimes (i numcols)
-      (insert "c|"))
-    (insert "}\n    \\hline\n")
-    ;; header row
-    (insert "    \\\\\\hline\\hline\n")
-    ;; first data row
-    (insert "    \\\\\\hline\n")
-    (insert "  \\end{tabular}\n  \\tableSpace\n")
-    (if (> (length label) 0)
-      (insert (format "  \\label{tab:%s}\n" label)))
-    (insert "\\end{table}\n\n")
+  (let ((pos (point))
+	(col-spec (mapconcat 'identity (make-vector (+ numcols 1) "|") "c"))
+	(label-tex (if (> (length label) 0)
+		   (format "  \\label{tab:%s}\n" label) "")))
+    (insert (format
+"\\begin{table}
+  \\centering
+  \\caption{%s}
+  \\begin{tabular}{%s}\\\\\\hline
+    \\\\\\hline\\hline
+    \\\\\\hline
+  \\end{tabular}
+  \\tableSpace
+%s\\end{table}\n\n" caption col-spec label-tex))
     (goto-char pos)
-    (forward-line 5)
+    (forward-line 4)
     (forward-char 4)))
 
-;  (end-of-line -2)
-;  (backward-char 1))
 
-
+(defun gmu-graphics-path (graphics-path)
+  "Inserts the path name of a graphics file without the extension.
+   The file path will be relative to *gmu-latex-root*."
+  (interactive (list
+		 (file-name-sans-extension
+		   (file-relative-name (read-file-name "File name:"
+						       *gmu-latex-fig-dir*)
+				       *gmu-latex-root*))))
+  (insert graphics-path))
 
